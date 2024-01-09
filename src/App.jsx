75cdea2d05a3e15beaa8a1fcf8a6c185432689c3
Anjo Pagdanganan <anjopag31@gmail.com>
2024-01-09 01:26:15 -0800

    trying to fix the ports

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import SpotifyWebApi from "spotify-web-api-js";
 import 'axios';
 
-const BACKEND_ROUTE = "http://localhost:8888"
+const BACKEND_ROUTE = "https://snobbify-backend.onrender.com"
 
 const spotifyApi = new SpotifyWebApi();
 
